36da964fc1141b78fc3f9a8cc4767c4df2a995bc
shawn <shawndaix@outlook.com>
2023-02-02 14:44:38 -0500

    First commit

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string message = "Hello World! This is Xun Dai";
+            Console.WriteLine(message);
+            Console.Readkey();
+
+
+        }
+    }
+}
