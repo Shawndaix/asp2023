675b7f9f49917871d9715bd79ae119a2e452b395
shawn <shawndaix@outlook.com>
2023-02-06 22:20:42 -0500

    day5 class

---
+++ day5/Cstdemo1/Cstdemo1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cstdemo1
+{
+    public class Person
+    {
+        public string _name; // class person, one field(_name).
+        public string _gender;
+        public int _age;
+
+        public void M1()
+        {
+            Console.WriteLine("This is not static method of the person!!");
+        }
+
+        public static void M2()
+        {
+            Console.WriteLine("This is static method of the person!");
+        }
+    }
+}
