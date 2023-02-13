cb9c64c41756ad694e25f3de5b94134c7c2887bd
shawn <shawndaix@outlook.com>
2023-02-13 01:08:36 -0500

    list

---
+++ day9/Cstday9/CstDemo6/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CstDemo6
+{
+    public class Person
+    {
+        //private string _name;
+
+        //public string Name
+        //{
+        //    get { return _name; }
+        //    set { _name = value; }
+        //}
+
+        private string _eyecolor;
+
+        public string Eyecolor
+        {
+            get { return _eyecolor; }
+            set { _eyecolor = value; }
+        }
+
+        public string Name { get; set; } // 自动属性
+
+        //public int MyProperty { get; private set; }
+
+    }
+}
